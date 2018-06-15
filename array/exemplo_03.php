957d7132e19225ad9d637da9f47c6b0acd7d863a
Anderson Munhoz <anderson.munhoz@webjump.com.br>
2018-06-15 13:03:39 -0300

    array-1

---
+++ array/exemplo_03.php
@@ -0,0 +1,23 @@
+<?php 
+
+$pessoas = array();
+
+array_push($pessoas, array(
+
+	'nome'=>'Joao',
+	'idade' => 38
+
+));
+
+
+array_push($pessoas, array(
+
+	'nome'=>'Maria',
+	'idade' => 41
+
+));
+
+print_r($pessoas[0]['nome']);
+
+
+ ?>
