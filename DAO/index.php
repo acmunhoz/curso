98015ed31a37231dcdf582da150cd74889dddb3b
Anderson Munhoz <anderson.munhoz@webjump.com.br>
2018-07-05 13:21:08 -0300

    DAO List

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,25 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega 1 usuário
+//$root = new Usuario();
+//$root->loadById(8);
+//echo $root;
 
-$root->loadById(8);
 
-echo $root;
+//Carrega uma list de usuários
+
+//$lista = new Usuario();
+
+//$lista->getList();
+
+//echo json_encode($lista);
+
+
+//Carrega uma lista com usuarios buscando pelo login
+
+$search = Usuario::search("ch");
+
+echo json_encode($search);
 
  ?>
